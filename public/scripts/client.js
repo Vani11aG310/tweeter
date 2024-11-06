b65f5177ef4ce454a196a675c50b72b5ccb92acd
Angelo <angvil02@gmail.com>
2024-11-06 15:07:24 -0800

    Added the ability to dynamically render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+createTweetElement = function(tweet) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="user">
+          <img src=${tweet.user.avatars}>
+          <span>${tweet.user.name}</span>
+        </div>
+        <span style="color:darkgrey">${tweet.user.handle}</span>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <span>${tweet.created_at}</span>
+        <section class="tweet-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-sharp fa-solid fa-heart"></i>
+        </section>
+      </footer>
+    </article>
+  `)
+  return $tweet;
+}
+
+renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const newTweet = createTweetElement(tweet);
+    $('.tweet-library').append(newTweet);
+  }
+}
+
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+renderTweets(data);
