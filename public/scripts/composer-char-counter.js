961d398225b4f77a3276ca1128c3b8794173ec0c
Angelo <angvil02@gmail.com>
2024-11-06 11:52:39 -0800

    added html css for the regular and hover states of tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   $(".new-tweet").on("input", "#tweet-text", function() {
-    if (140 - this.value.length <= 0) {
-      $('.counter').replaceWith(`<output name="counter" class="counter" for="tweet-text" style="color: red">${140 - this.value.length}</output>`)
+    var remainingChars = 140 - this.value.length;
+    $('.counter').text(remainingChars);
+
+    if (remainingChars < 0) {
+      $('.counter').css("color", "red");
     } else {
-      $('.counter').replaceWith(`<output name="counter" class="counter" for="tweet-text">${140 - this.value.length}</output>`)
+      $('.counter').css("color", "");
     }
   })
 })
