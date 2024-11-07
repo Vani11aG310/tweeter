bedcf1af80bac39aa12ea00bd9e5e6b4956b4ad6
Angelo <angvil02@gmail.com>
2024-11-06 16:02:29 -0800

    Tweets are now being dynamically created from the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,62 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-createTweetElement = function(tweet) {
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <div class="user">
-          <img src=${tweet.user.avatars}>
-          <span>${tweet.user.name}</span>
-        </div>
-        <span style="color:darkgrey">${tweet.user.handle}</span>
-      </header>
-      <p>${tweet.content.text}</p>
-      <footer>
-        <span>${tweet.created_at}</span>
-        <section class="tweet-icons">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-sharp fa-solid fa-retweet"></i>
-          <i class="fa-sharp fa-solid fa-heart"></i>
-        </section>
-      </footer>
-    </article>
-  `)
-  return $tweet;
-}
-
-renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    const newTweet = createTweetElement(tweet);
-    $('.tweet-library').prepend(newTweet);
+$(document).ready( function() {
+  createTweetElement = function(tweet) {
+    const dateCreated = tweet.created_at
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="user">
+            <img src=${tweet.user.avatars}>
+            <span>${tweet.user.name}</span>
+          </div>
+          <span style="color:darkgrey">${tweet.user.handle}</span>
+        </header>
+        <p>${tweet.content.text}</p>
+        <footer>
+          <span>${timeago.format(dateCreated)}</span>
+          <section class="tweet-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-sharp fa-solid fa-retweet"></i>
+            <i class="fa-sharp fa-solid fa-heart"></i>
+          </section>
+        </footer>
+      </article>
+    `)
+    return $tweet;
   }
-}
-
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+  
+  renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $('.tweet-library').prepend(newTweet);
     }
-  ]
+  }
 
-renderTweets(data);
+  loadTweets = function() {
+    $.get('/tweets', function(data) {
+      renderTweets(data)
+    })
+  }
+
+  loadTweets();
+})
