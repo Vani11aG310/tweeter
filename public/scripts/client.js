5c98424d23956eee61f12124717d2cbcf3661be4
Angelo <angvil02@gmail.com>
2024-11-07 19:24:04 -0800

    Cleaned code for final submission. Removed some css selectors as they were scoring too high in specificity and overriding parent styles being passed down. Added comments for explanation in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready( function() {
+$(document).ready(function() {
   $('.tweet-form').on('submit', function(event) {
     const tweet = $('#tweet-text').val();
-    const trimmedTweet = tweet.trim()
-    const validation = isTweetValid(trimmedTweet)
+    const trimmedTweet = tweet.trim();
+    const validation = isTweetValid(trimmedTweet);
     event.preventDefault();
     if (!validation.valid && validation.errorMessage) {
       $('#er').html(`<i class="fa-solid fa-triangle-exclamation"></i> ${validation.errorMessage} <i class="fa-solid fa-triangle-exclamation"></i>`);
-      $('#er').slideDown("fast", function() {});
+      $('#er').slideDown("fast", function() { });
     } else {
-      $('#er').slideUp("fast", function() {});
+      $('#er').slideUp("fast", function() { });
       $('#tweet-text').val(trimmedTweet);
       const formData = $(this).serialize();
       $.ajax({
         type: "POST",
         url: "/tweets",
         data: formData,
-        success: function () {
+        success: function() {
           $('#tweet-text').val("");
-          $('.counter').text('140')
+          $('.counter').text('140');
           loadTweets();
         }
-      })
+      });
     }
-  })
-
+  });
+  // validate submission before passing to GET request
   function isTweetValid(tweet) {
     let valid = true;
     let errorMessage;
@@ -42,16 +42,16 @@ $(document).ready( function() {
       errorMessage = "Your tweet is too long. Please keep it within our character limit of 140";
     }
 
-    return { valid, errorMessage }; 
+    return { valid, errorMessage };
   }
-
+  // create html for tweet
   function createTweetElement(tweet) {
     const dateCreated = timeago.format(escape(tweet.created_at));
     const text = escape(tweet.content.text);
     const handle = escape(tweet.user.handle);
     const name = escape(tweet.user.name);
-    const avatarURL = escape(tweet.user.avatars)
-   
+    const avatarURL = escape(tweet.user.avatars);
+
     const $tweet = $(`
       <article class="tweet">
         <header>
@@ -71,10 +71,10 @@ $(document).ready( function() {
           </section>
         </footer>
       </article>
-    `)
+    `);
     return $tweet;
   }
-  
+  // Render tweets on the page
   function renderTweets(tweets) {
     $('.tweet-library').empty();
     for (const tweet of tweets) {
@@ -82,16 +82,16 @@ $(document).ready( function() {
       $('.tweet-library').prepend(newTweet);
     }
   }
-
+  // fetch tweets from server
   function loadTweets() {
     $.ajax('/tweets', {
       method: 'GET'
     })
-    .then(function(data) {
-      renderTweets(data);
-    })
+      .then(function(data) {
+        renderTweets(data);
+      });
   }
-
+  // protection function from cross-site scripting attacks, escaping harmful characters.
   function escape(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
@@ -99,4 +99,4 @@ $(document).ready( function() {
   }
 
   loadTweets();
-})
+});
